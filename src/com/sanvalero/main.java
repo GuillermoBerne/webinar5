9122c5646516f13dadfb137fe2cee47c15d03bdb
GuillermoBerne <guillermoberne@gmail.com>
2022-02-14 20:41:59 +0100

    version0.1

--- src/com/sanvalero/main.java
+++ src/com/sanvalero/main.java
@@ -3,5 +3,6 @@ package com.sanvalero;
 public class main {
     public static void main(String[] args){
         System.out.println("WEBINAR 5 GIT/GITHUB");
+        System.out.println("Version 0.1");
     }
 }
